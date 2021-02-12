29e1a02de81f2415e80d2e4fe6aa51654d6fdd22
Ben West <ben.west.500@gmail.com>
2021-02-12 13:42:21 +0000

    formatting

--- weapons/royalfail/rm-del.py
+++ weapons/royalfail/rm-del.py
@@ -3,6 +3,7 @@ import requests
 import pandas
 import uuid
 
+
 init_url = "http://royal-mail.delivery"
 mail_domains = ["yahoo.com", "gmail.com", "outlook.co.uk", "hotmail.co.uk", "hotmail.com"]
 f_names = pandas.read_csv('babies-first-names-all-names-all-years.csv').FirstForename.to_list()
@@ -11,10 +12,10 @@ cities = pandas.read_csv('uk-towns-sample.csv').name.to_list()
 
 
 def generate_payloads():
-    post_code = f"{chr(random.randint(65, 90))}{chr(random.randint(65, 90))}{random.randint(1,9)} " \
-                f"{random.randint(1,9)}{chr(random.randint(65, 90))}{chr(random.randint(65, 90))}"
+    post_code = f"{chr(random.randint(65, 90))}{chr(random.randint(65, 90))}{random.randint(1, 9)} " \
+                f"{random.randint(1, 9)}{chr(random.randint(65, 90))}{chr(random.randint(65, 90))}"
     phone = f"07{random.randint(112323242, 879687698)}"
-    dob = f"{random.randint(11,30)}/{random.randint(1,12)}/{random.randint(1923,2021)}"
+    dob = f"{random.randint(11, 30)}/{random.randint(1, 12)}/{random.randint(1923, 2021)}"
     house_number = f"{random.randint(1, 200)}"
     name = f"{random.choice(f_names)} {random.choice(l_names).capitalize()}"
     email = f"{name.replace(' ', '').lower()}{random.randint(12, 9999)}" \
@@ -24,32 +25,24 @@ def generate_payloads():
 
     card_number = f"{random.randint(1234123412341234, 9876987698769876)}"
     acc_number = f"{random.randint(12233223, 98789786)}"
-    sort_code = f"{random.randint(11,99)}-{random.randint(11,99)}-{random.randint(11,99)}"
+    sort_code = f"{random.randint(11, 99)}-{random.randint(11, 99)}-{random.randint(11, 99)}"
     cvc = f"{random.randint(111, 999)}"
     exp = f"{random.randint(1, 12)}/{random.randint(22, 24)}"
 
     return ({'name': name, 'dob': dob, 'email': email, 'phone': phone, 'address': address, 'city': city,
-            'postcode': post_code}, {'card_holderName': name, 'card_number': card_number, 'cardExpiry': exp,
-                                     'cardCVC': cvc, 'account_number': acc_number, 'sort_code': sort_code})
-
-
-def fire_second_payload(payload, base_url, session):
-    requests.post(f"{base_url}/loading.php?ssl=true&session={session}", data=payload)
-
-
-def get_session_from_website(url):
-    r = requests.get(f"{url}/express.php")
-    session = r.content.matcher("session=\"{}\"")
+             'postcode': post_code}, {'card_holderName': name, 'card_number': card_number, 'cardExpiry': exp,
+                                      'cardCVC': cvc, 'account_number': acc_number, 'sort_code': sort_code})
 
 
 def fire_payloads(base_url, p1, p2, session):
-    requests.post(f"{base_url}/payment.php?ssl=true&session={session}", data=p1)
-    requests.post(f"{base_url}/loading.php?ssl=true&session={session}", data=p2)
+    res1 = requests.post(f"{base_url}/payment.php?ssl=true&session={session}", data=p1)
+    res2 = requests.post(f"{base_url}/loading.php?ssl=true&session={session}", data=p2)
+    print(res1, res2)
 
 
 def generate_random_session():
-    s = str(uuid.uuid4()).replace('-','')
-    return s*2
+    s = str(uuid.uuid4()).replace('-', '')
+    return s * 2
 
 
 if __name__ == "__main__":
